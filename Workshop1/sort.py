a097044fc0c2f11ac99f9229265c48ac3bc0bd59
Herrera <shg@pop-os.localdomain>
2022-07-25 21:11:59 -0500

    First commit

--- Workshop1/sort.py
+++
@@ -1,8 +0,0 @@
-import random
-array = [random.randint(0, 1000)for i in range(0, 10)]
-for i in range(0, len(array)):
-    for j in range(0, len(array)):
-        if array[i] < array[j]:
-            temp = array[j]
-            array[j] = array[i]
-            array[i] = temp
