788236c16734ef06d0f5de9edf0ef944ba892f10
jsebastianherrera <juansehgu@gmail.com>
2022-08-17 22:00:27 -0500

    cpp updated

--- Workshop2/binary.cpp
+++ Workshop2/binary.cpp
@@ -1,26 +1,41 @@
+#include <climits>
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <stdlib.h>
+#include <chrono>
+#include <cmath>
 using namespace std;
-vector<int> binary(int n);
-void reverse_rotate(string::iterator first, string::iterator last);
-int main()
+typedef std::chrono::high_resolution_clock::time_point TimeVar;
+#define duration(a) std::chrono::duration_cast<std::chrono::nanoseconds>(a).count() * 0.000000001
+#define timeNow() std::chrono::high_resolution_clock::now()
+template <typename F, typename... Args>
+double funcTime(F func, Args &&...args)
 {
-    int n = 309;
-    vector<int> S = binary(n);
-    std::string s;
-    // Print inverted binary
-    cout << "ITE: ";
-    for (int i = 0; i < S.size(); i++)
-        cout << S[i];
-    // Invert inverted_binary
-    std::vector<int>::reverse_iterator it;
-    for (it = S.rbegin(); it != S.rend(); it++)
-        s += to_string(*it);
-    reverse_rotate(s.begin(), s.end());
-    cout << "\nREC: " << s << endl;
+    TimeVar t1 = timeNow();
+    func(std::forward<Args>(args)...);
+    return duration(timeNow() - t1);
 }
-vector<int> binary(int n)
+vector<int> binary(unsigned long long int n);
+void reverse_rotate(string::iterator first, string::iterator last);
+int main(int argc, char **argv)
+{
+    // exe min max step
+    unsigned long long int min = 1;
+    int max = 64;
+  
+    for (int i = 1; i <= max; i++)
+    {
+        vector<int> S = binary(min);
+        std::string s;
+        std::vector<int>::reverse_iterator it;
+        for (it = S.rbegin(); it != S.rend(); it++)
+            s += to_string(*it);
+        std::cout << s << "," << min << "," << funcTime(binary, min) << "," << funcTime(reverse_rotate, s.begin(), s.end()) << endl;
+        min = min * 2;
+    }
+}
+vector<int> binary(unsigned long long int n)
 {
     vector<int> vec;
     while (n / 2 > 0)
